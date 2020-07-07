459879218352460ca0ecbb00d984a1ec141fb86c
Andrey <44496797+Andrew-Causelof@users.noreply.github.com>
2020-07-07 14:15:59 +0300

    added skills and expirience section

--- js/script.js
+++ js/script.js
@@ -6,11 +6,77 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typed", {
-        strings: ["3D Designer.", "Software Enineer.", "Web Developer.", "Product Manager.", ],
+        strings: ["3D Designer.",
+        "Software Enineer.",
+         "Web Developer.", 
+         "Product Manager.",
+         "CAD Designer..." ],
         typeSpeed: 100,
         loop: true,
         startDelay: 1000,
         showCursor: false
     });
-});
 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:4,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            939:{
+                items:4
+            },
+            1200:{
+                items:6
+            },
+
+        }
+    });
+
+
+
+    var skillsTopOffset =$(".skillsSection").offset().top;
+    var statsTopOffset =$(".statsSection").offset().top;
+    var countUpFinished = false;
+
+    //console.log(skillsTopOffset);
+    
+    $(window).scroll(function(){
+       
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                //scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent){
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+
+        }
+
+        if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+
+            $(".counter").each(function(){
+                var element = $(this);
+                var endVal = parseInt(element.text());
+                element.countup(endVal);
+            })
+
+            countUpFinished = true;
+
+        }
+    });
+});
