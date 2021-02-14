2f013ac007484ca92feac13c585b6989332adee1
Andrey <44496797+Andrew-Causelof@users.noreply.github.com>
2021-02-15 00:39:30 +0300

    added php logic to lvito site and filled first cards

--- include/portfolio.php
+++ include/portfolio.php
@@ -1,10 +1,10 @@
 <?php
 return [
     1 => [
-        'thumbnail' => 'img/portfolio/SevenVoices/thumbnails/SevenVoices.png',
-        'layout'    => 'img/portfolio/SevenVoices/SevenVoices_layout.png',
-        'data'      => 'Seven Voices Rock Band',
-        'link'      => 'https://andrew-causelof.github.io/the-band/',
+        'thumbnail' => 'img/portfolio/moscow-city/thumbnail.png',
+        'layout'    => 'img/portfolio/moscow-city/layout.png',
+        'data'      => 'Moscow City Guide',
+        'link'      => 'https://moscow-city.guide',
         'class'     => 'webSites'
         ],
     2 => [
@@ -22,227 +22,227 @@ return [
         'class'     => 'webGl'
     ],
     4 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Breakout-Game/thumbnail.png',
+        'layout'    => 'img/portfolio/Breakout-Game/layout.png',
         'data'      => 'Breakout-Game',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Breakout-Game/index.html',
         'class'     => 'shortJSapps'
     ],
     5 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Custom-Video-Player/thumbnail.png',
+        'layout'    => 'img/portfolio/Custom-Video-Player/layout.png',
         'data'      => 'Customized Video Player',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Custom-Video-Player/index.html',
         'class'     => 'shortJSapps'
     ],
     6 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/DOM-array-methods/thumbnail.png',
+        'layout'    => 'img/portfolio/DOM-array-methods/layout.png',
         'data'      => 'practiced with common DOM array methods',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/DOM-array-methods/index.html',
         'class'     => 'shortJSapps'
     ],
     7 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Drag-and-drop/thumbnail.png',
+        'layout'    => 'img/portfolio/Drag-and-drop/layout.png',
         'data'      => 'js drag and drop',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Drag-and-drop/index.html',
         'class'     => 'shortJSapps'
     ],
     8 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Exchange-Rate-Calculator/thumbnail.png',
+        'layout'    => 'img/portfolio/Exchange-Rate-Calculator/layout.png',
         'data'      => 'Exchange-Rate-Calculator',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Exchange-Rate-Calculator/index.html',
         'class'     => 'shortJSapps'
     ],
     9 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Expense-Tracker/thumbnail.png',
+        'layout'    => 'img/portfolio/Expense-Tracker/layout.png',
         'data'      => 'Expense-Tracker',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Expense-Tracker/index.html',
         'class'     => 'shortJSapps'
     ],
     10 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Form-Validator/thumbnail.png',
+        'layout'    => 'img/portfolio/Form-Validator/layout.png',
         'data'      => 'Form-Validator',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Form-Validator/index.html',
         'class'     => 'shortJSapps'
     ],
     11 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Form-Infinite-Scroll-Posts/thumbnail.png',
+        'layout'    => 'img/portfolio/Form-Infinite-Scroll-Posts/layout.png',
         'data'      => 'Infinite-Scroll-Posts',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Infinite-Scroll-Posts/index.html',
         'class'     => 'shortJSapps'
     ],
     12 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/JS-Game-Snake/thumbnail.png',
+        'layout'    => 'img/portfolio/JS-Game-Snake/layout.png',
         'data'      => 'JS-Game-Snake',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/JS-Game-Snake/index.html',
         'class'     => 'shortJSapps'
     ],
     13 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/JS-animation-Lightning-for-Logo/thumbnail.png',
+        'layout'    => 'img/portfolio/JS-animation-Lightning-for-Logo/layout.png',
         'data'      => 'JS-animation-Lightning-for-Logo',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/JS-animation-Lightning-for-Logo/index.html',
         'class'     => 'webGl'
     ],
     14 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/JS-animation-graph/thumbnail.png',
+        'layout'    => 'img/portfolio/JS-animation-graph/layout.png',
         'data'      => 'JS-animation-graph',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/JS-animation-graph/index.html',
         'class'     => 'webGl'
     ],
     15 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/JS-animation-moving-dots/thumbnail.png',
+        'layout'    => 'img/portfolio/JS-animation-moving-dots/layout.png',
         'data'      => 'JS-animation-moving-dots',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/JS-animation-moving-dots/index.html',
         'class'     => 'webGl'
     ],
     16 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/JS-script-photo_blinds/thumbnail.png',
+        'layout'    => 'img/portfolio/JS-script-photo_blinds/layout.png',
         'data'      => 'JS-script-photo_blinds',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/JS-script-photo_blinds/index.html',
         'class'     => 'shortJSapps'
     ],
     17 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Lyrics-Search-App/thumbnail.png',
+        'layout'    => 'img/portfolio/Lyrics-Search-App/layout.png',
         'data'      => 'Lyrics-Search-App',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Lyrics-Search-App/index.html',
         'class'     => 'shortJSapps'
     ],
     18 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Meal-Finder/thumbnail.png',
+        'layout'    => 'img/portfolio/Meal-Finder/layout.png',
         'data'      => 'Meal-Finder',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Meal-Finder/index.html',
         'class'     => 'shortJSapps'
     ],
     19 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Memory-cards/thumbnail.png',
+        'layout'    => 'img/portfolio/Memory-cards/layout.png',
         'data'      => 'Memory-cards',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Memory-cards/index.html',
         'class'     => 'shortJSapps'
     ],
     20 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Menu-Slider/thumbnail.png',
+        'layout'    => 'img/portfolio/Menu-Slider/layout.png',
         'data'      => 'Menu-Slider',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Menu-Slider/index.html',
         'class'     => 'shortJSapps'
     ],
     21 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Movie-Seat-Booking/thumbnail.png',
+        'layout'    => 'img/portfolio/Movie-Seat-Booking/layout.png',
         'data'      => 'Movie-Seat-Booking',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Movie-Seat-Booking/index.html',
         'class'     => 'shortJSapps'
     ],
     22 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Music-Player/thumbnail.png',
+        'layout'    => 'img/portfolio/Music-Player/layout.png',
         'data'      => 'Music-Player',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Music-Player/index.html',
         'class'     => 'shortJSapps'
     ],
     23 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/New-Year-Countdown/thumbnail.png',
+        'layout'    => 'img/portfolio/New-Year-Countdown/layout.png',
         'data'      => 'New-Year-Countdown',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/New-Year-Countdown/index.html',
         'class'     => 'shortJSapps'
     ],
     24 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Relaxer-App/thumbnail.png',
+        'layout'    => 'img/portfolio/Relaxer-App/layout.png',
         'data'      => 'Relaxer-App',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Relaxer-App/index.html',
         'class'     => 'shortJSapps'
     ],
     25 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Typing-Game/thumbnail.png',
+        'layout'    => 'img/portfolio/Typing-Game/layout.png',
         'data'      => 'Typing-Game',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/Typing-Game/index.html',
         'class'     => 'shortJSapps'
     ],
     26 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/hangman-Game/thumbnail.png',
+        'layout'    => 'img/portfolio/hangman-Game/layout.png',
         'data'      => 'hangman-Game',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/hangman-Game/index.html',
         'class'     => 'shortJSapps'
     ],
     27 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/speak-number-game/thumbnail.png',
+        'layout'    => 'img/portfolio/speak-number-game/layout.png',
         'data'      => 'speak-number-game',
         'link'      => 'http://andrew-causelof.github.io/practiceJS/speak-number-game/index.html',
         'class'     => 'shortJSapps'
     ],
     28 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/admin-panel/thumbnail.png',
+        'layout'    => 'img/portfolio/admin-panel/layout.png',
         'data'      => 'Admin panel',
         'link'      => 'https://andrew-causelof.github.io/admin-panel/',
         'class'     => 'webSites'
     ],
     29 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/realtor/thumbnail.png',
+        'layout'    => 'img/portfolio/realtor/layout.png',
         'data'      => 'Realtor site',
         'link'      => 'https://andrew-causelof.github.io/realtor/',
         'class'     => 'webSites'
     ],
     30 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio//mom-s-cake/thumbnail.png',
+        'layout'    => 'img/portfolio//mom-s-cake/layout.png',
         'data'      => 'Mom-s-cake',
         'link'      => 'https://andrew-causelof.github.io/mom-s-cake/',
         'class'     => 'webSites'
     ],
     31 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/static-bottle-photo/thumbnail.png',
+        'layout'    => 'img/portfolio/static-bottle-photo/layout.png',
         'data'      => 'Image filter displace',
         'link'      => 'http://causelof.ru/threejs/static-bottle-photo/',
         'class'     => 'webGl'
     ],
     32 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/bottle-parallax/thumbnail.png',
+        'layout'    => 'img/portfolio/bottle-parallax/layout.png',
         'data'      => 'Bottle and floating leaves',
         'link'      => 'http://causelof.ru/threejs/bottle-parallax/',
         'class'     => 'webGl'
     ],
     33 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/bottles-in-row/thumbnail.png',
+        'layout'    => 'img/portfolio/bottles-in-row/layout.png',
         'data'      => 'Bottle different glass colors',
         'link'      => 'http://causelof.ru/threejs/bottles-in-row/',
         'class'     => 'webGl'
     ],
     34 => [
-        'thumbnail' => 'img/portfolio/spinlife/thumbnail/spinlife_small.png',
-        'layout'    => 'img/portfolio/spinlife/spinlife_big.png',
+        'thumbnail' => 'img/portfolio/Desk-and-bottle/thumbnail.png',
+        'layout'    => 'img/portfolio/Desk-and-bottle/layout.png',
         'data'      => 'Bar desk and bottle scene',
         'link'      => 'http://causelof.ru/threejs/Desk-and-bottle/',
         'class'     => 'webGl'
-    ]
-    
-
-
-
-
-         
-  
+    ],
+    35 => [
+        'thumbnail' => 'img/portfolio/SevenVoices/thumbnails/SevenVoices.png',
+        'layout'    => 'img/portfolio/SevenVoices/SevenVoices_layout.png',
+        'data'      => 'Seven Voices Rock Band',
+        'link'      => 'https://andrew-causelof.github.io/the-band/',
+        'class'     => 'webSites'
+        ] 
 ];?>
